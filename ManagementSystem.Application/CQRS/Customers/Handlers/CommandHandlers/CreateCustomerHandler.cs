5c64070ccd91673c935f9ff2cacc43c58e267e87
Ali Aliyev <eli.eliyev.200424@gmail.com>
2025-04-07 23:33:33 +0400

    Initial commit

---
+++ ManagementSystem.Application/CQRS/Customers/Handlers/CommandHandlers/CreateCustomerHandler.cs
@@ -0,0 +1,38 @@
+﻿using ManagementSystem.Application.CQRS.Customers.Commands.Requests;
+using ManagementSystem.Application.CQRS.Customers.Commands.Responses;
+using ManagementSystem.Common.Exceptions;
+using ManagementSystem.Common.GlobalResponses;
+using ManagementSystem.Common.GlobalResponses.Generics;
+using ManagementSystem.Domain.Entities;
+using ManagementSystem.Repository.Common;
+using MediatR;
+
+namespace ManagementSystem.Application.CQRS.Customers.Handlers.CommandHandlers;
+
+public class CreateCustomerHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateCustomerRequest, Result<CreateCustomerResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    public async Task<Result<CreateCustomerResponse>> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+    {
+        Customer newCustomer = new(request.Name, request.Email);
+
+        if(string.IsNullOrEmpty(newCustomer.Name))
+        {
+            throw new BadRequestException("Categoriyanin adi null ve ya bosh ola bilmez");
+        }
+
+        await _unitOfWork.CustomerRepository.AddAsync(newCustomer);
+        CreateCustomerResponse response = new()
+        {
+            Id = newCustomer.Id,
+            Name = newCustomer.Name,
+            Email = newCustomer.Email
+        };
+        return new Result<CreateCustomerResponse>
+        {
+            Data = response,
+            Errors = [],
+            IsSuccess = true
+        };
+    }
+}
---
+++ ManagementSystem.Application/CQRS/Customers/Handlers/QueryHandlers/GetAllCustomerHandler.cs
@@ -0,0 +1,26 @@
+﻿using ManagementSystem.Application.CQRS.Customers.Queries.Requests;
+using ManagementSystem.Application.CQRS.Customers.Queries.Responses;
+using ManagementSystem.Common.GlobalResponses;
+using ManagementSystem.Common.GlobalResponses.Generics;
+using ManagementSystem.Repository.Common;
+using MediatR;
+
+namespace ManagementSystem.Application.CQRS.Customers.Handlers.QueryHandlers;
+
+public class GetAllCustomerHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetAllCustomerRequest, Result<List<GetAllCustomerResponse>>>
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task<Result<List<GetAllCustomerResponse>>> Handle(GetAllCustomerRequest request, CancellationToken cancellationToken)
+    {
+        var customers = await _unitOfWork.CustomerRepository.GetAllInitialDataAsync();
+        var response = customers.Select(x => new GetAllCustomerResponse
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Email = x.Email,
+        }).ToList();
+
+        return new Result<List<GetAllCustomerResponse>>() { Data = response, IsSuccess = true };
+    }
+}
